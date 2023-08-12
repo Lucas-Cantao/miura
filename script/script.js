fffdc07f93268f8dbb7e79c78a6822feab1e44a0
Lucas Cantão <124742219+Lucas-Cantao@users.noreply.github.com>
2023-08-12 11:52:33 -0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,16 @@
+let test = 0;
+
+function showContent(id)
+
+{
+    let content = document.querySelector(`#conteudo-${id}`);
+
+    if(test == 0)
+    {
+        content.style.display = "block";
+        test = 1;
+    }else{
+        content.style.display = "none";
+        test = 0;
+    };
+};
